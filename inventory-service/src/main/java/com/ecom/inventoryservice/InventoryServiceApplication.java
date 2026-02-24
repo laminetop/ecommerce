a6a8ef3829fcbfa9f81174664de951412997fdd2
Amadou TOP <toplamzo10@gmail.com>
2026-02-24 01:11:17 -0500

    commit sur master : Configuration dynamique du gateway

--- inventory-service/src/main/java/com/ecom/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ecom/inventoryservice/InventoryServiceApplication.java
@@ -1,8 +1,12 @@
 package com.ecom.inventoryservice;
 
+import com.ecom.inventoryservice.entities.Product;
+import com.ecom.inventoryservice.reporitories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,23 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(Product.builder()
+                    .nom("ordinateur portable")
+                    .price(1240.0)
+                    .quantite(11).build());
+
+            productRepository.save(Product.builder()
+                    .nom("Ipad SamsungGalaxy")
+                    .price(900.0)
+                    .quantite(5).build());
+
+            productRepository.save(Product.builder()
+                    .nom("Imprimante Lazer Epson")
+                    .price(600)
+                    .quantite(15).build());
+        };
+    }
 }
---
+++ inventory-service/src/main/java/com/ecom/inventoryservice/entities/Product.java
@@ -0,0 +1,19 @@
+package com.ecom.inventoryservice.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Product {
+
+    @Id @GeneratedValue
+    private  Long id ;
+    private String nom;
+    private double price;
+    private  int quantite;
+
+}
